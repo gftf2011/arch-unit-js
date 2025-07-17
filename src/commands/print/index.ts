579ce88a44992241648cdcae1d72d56f0a8beefc
Gabriel Ferrari Tarallo Ferraz <gftf2011@gmail.com>
2025-07-17 10:06:58 -0300

    refactor: change ast files

--- src/commands/print/index.ts
+++
@@ -1,6 +0,0 @@
-import { astParser } from "../../core/parser/ASTParser";
-
-export const printCommand = (normalized: boolean) => {
-  const tree = astParser(normalized);
-  console.log(JSON.stringify(tree, null, 2));
-};
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,35 @@
 #!/usr/bin/env node
 
-import { astParser } from './core/parser/ASTParser';
-
+import path from 'path';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
-import { printCommand } from './commands/print';
+import { ast } from './core/ast';
 
 yargs(hideBin(process.argv))
   .scriptName('arch-unit')
-  .command('print', 'Print the AST of the project', (yargs) => {
-    yargs.option('normalized', {
+  .command('print <dirPath>', 'Print the AST of the project', (yargs) => {
+    yargs
+    .positional('dirPath', {
+      type: 'string',
+      describe: 'The path to the project to be printed',
+      demandOption: true,
+    })
+    .option('normalized', {
       alias: 'n',
       type: 'boolean',
       default: false,
       describe: 'Print full normalized (absolute) paths from "files dependencies"'
     });
   }, (argv) => {
+    const dirPath = argv.dirPath as string;
+    if (!dirPath) {
+      console.error('Please provide a valid <dirPath> argument');
+      process.exit(1);
+    }
+    const projectPath = path.resolve(process.cwd(), dirPath);
     const normalized = argv.normalized as boolean;
-    printCommand(normalized);
+    const tree = ast.tree.generate(projectPath, normalized);
+    console.log(JSON.stringify(tree, null, 2));
   })
   .help()
   .alias('h', 'help')
