5841e7f530c1dd39dbb6ee6b7abb102ec216a2d8
Gabriel Ferrari Tarallo Ferraz <gftf2011@gmail.com>
2025-08-04 20:43:55 -0300

    test: add more normalization

--- src/core/dependency/index.ts
+++ src/core/dependency/index.ts
@@ -63,13 +63,13 @@ class JavascriptOrTypescriptRelatedDependency extends Dependency {
         const candidates = [
             resolvedPathForCandidates,
             ...extensions.map(ext => `${resolvedPathForCandidates}${ext}`),
-            ...extensions.map(ext => path.join(resolvedPathForCandidates, `index${ext}`)),
+            ...extensions.map(ext => normalizeWindowsPath(path.join(resolvedPathForCandidates, `index${ext}`))),
         ];
     
         for (const candidate of candidates) {
             try {
                 const stat = fs.statSync(candidate);
-                if (stat.isFile()) return new JavascriptOrTypescriptRelatedDependency(path.relative(rootDir, candidate), candidate, 'valid-path', resolvedWith, comesFrom);
+                if (stat.isFile()) return new JavascriptOrTypescriptRelatedDependency(normalizeWindowsPath(path.relative(rootDir, candidate)), candidate, 'valid-path', resolvedWith, comesFrom);
             } catch {
                 // do nothing
             }
@@ -88,14 +88,6 @@ export class DependencyFactory {
         resolvedWith: DependencyResolvedWith,
         comesFrom: NodeDependencyComesFrom
     ): Dependency {
-
-        console.log('dependency', dependency);
-        console.log('currentPath', currentPath);
-        console.log('rootDir', rootDir);
-        console.log('extensions', extensions);
-        console.log('resolvedWith', resolvedWith);
-        console.log('comesFrom', comesFrom);
-
         if (comesFrom === 'javascript-or-typescript') {
             return JavascriptOrTypescriptRelatedDependency.create(rootDir, currentPath, dependency, extensions, resolvedWith, comesFrom);
         }
